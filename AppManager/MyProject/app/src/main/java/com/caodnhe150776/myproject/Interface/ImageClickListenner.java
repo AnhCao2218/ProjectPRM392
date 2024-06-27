2596e0121f52dfbcf01c4b36dbd5c8a7b01f9f96
AnhCao2218 <caodnhe150776@fpt.edu.vn>
2024-06-27 19:06:27 +0700

    AppManager

---
+++ AppManager/MyProject/app/src/main/java/com/caodnhe150776/myproject/Interface/ImageClickListenner.java
@@ -0,0 +1,7 @@
+package com.caodnhe150776.myproject.Interface;
+
+import android.view.View;
+
+public interface ImageClickListenner {
+    void onImageClick(View view, int pos, int giatri);
+}
---
+++ AppManager/MyProject/app/src/main/java/com/caodnhe150776/myproject/model/EventBus/TinhTongEvent.java
@@ -0,0 +1,4 @@
+package com.caodnhe150776.myproject.model.EventBus;
+
+public class TinhTongEvent {
+}
